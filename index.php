dc2822f1e308063aa0d80faf7182262144ab3658
Robert Arango <robertarango9@gmail.com>
2012-09-04 22:43:39 -0400

    Initial Upload
    Start of the Project

---
+++ index.php
@@ -0,0 +1,55 @@
+<?PHP include('php/private.php'); ?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml"><!-- InstanceBegin template="/OneDashboard/template/OneDashboard.dwt" codeOutsideHTMLIsLocked="false" -->
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+
+<!-- InstanceBeginEditable name="doctitle" -->
+<title>Untitled Document</title>
+<!-- InstanceEndEditable -->
+
+<link rel="stylesheet" type="text/css" href="css/style.css"/>
+
+<!-- InstanceBeginEditable name="head" -->
+<!-- InstanceEndEditable -->
+
+</head>
+
+<body>
+
+	<div id="header">
+    	
+        <div id="logo">
+        	<img src="../UnionDashboard/images/UnionDashboardLogo.png" alt="UnionDashboard Logo" />
+        </div>
+        <div id="nav-top">
+        	<ul>
+            	<li>Announcements</li>
+            	<li>Schedule</li>
+                <li>Checkout</li>
+                <li>AKS One</li>
+                <li>AKS Two</li>
+                <li>Settings</li>
+            </ul>
+        </div>
+    	
+    </div>
+    <div id="container">
+    
+        <div id="sidebar">
+            These will update depending on which section from the top you will be in
+        </div>
+        
+        <div id="content">
+            This will be the main interaction window for everyone
+        </div>
+        
+    </div>
+	
+    <div id="footer">
+    	Just a usual footer. nothing to see here.
+    </div>
+
+</body>
+<!-- InstanceEnd --></html>
